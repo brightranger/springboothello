06e2f67e8376c07684d5a898247f6fadda2d82e5
goodluck <332478519@qq.com>
2018-05-12 14:44:04 +0800

    第一次使用git提交代码

--- src/main/java/com/zhanghf/Application.java
+++ src/main/java/com/zhanghf/Application.java
@@ -11,6 +11,7 @@ public class Application {
     @RequestMapping("/")
     String home(){
         return "hello";
+
     }
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
